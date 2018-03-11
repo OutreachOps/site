b6149222892488484082dd526e0e4ec41c50ee98
Adrian Russell <adrian.f.russell@gmail.com>
2018-03-11 17:48:57 +1100

    add page with routing.

--- test/unit/specs/DefaultPage.js
+++ test/unit/specs/DefaultPage.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/components/DefaultPage'
 
-describe('HelloWorld.vue', () => {
+describe('DefaultPage.vue', () => {
   it('should render correct contents', () => {
     const Constructor = Vue.extend(HelloWorld)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.hello h1').textContent)
-      .toEqual('Welcome to Your Vue.js App')
+      .toEqual('Outlook Operations')
   })
 })
